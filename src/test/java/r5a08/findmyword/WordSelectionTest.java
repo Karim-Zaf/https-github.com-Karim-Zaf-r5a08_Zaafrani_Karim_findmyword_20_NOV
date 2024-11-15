6600dfffdb971b9652eebc486e46f51432a5fe15
bl <bouchkhaf@gmail.com>
2024-11-15 12:10:14 +0100

    some changes

--- src/test/java/r5a08/findmyword/WordSelectionTest.java
+++ src/test/java/r5a08/findmyword/WordSelectionTest.java
@@ -1,11 +1,12 @@
 package r5a08.findmyword;
 
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WordSelectionTest {
     @Mock
-
 
 }
